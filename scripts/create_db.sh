af9bcfa69d6b94040c9a73f9ca8eea841b50dbd9
maheshkkolla <mahesh.mk.245@gmail.com>
2015-06-25 20:49:08 +0530

    [Mahesh] Created script to create the db

---
+++ scripts/create_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "--> Clearing old data if exits"
+dropdb --if-exists coalsce
+echo "--> Creating DataBase ..."
+createdb coalsce
+echo "--> Created DataBase."
+echo "--> Creating Tables ..."
+psql coalsce -c "CREATE SEQUENCE user_id MINVALUE 1111"
+psql coalsce -c "CREATE TABLE users(
+	id integer PRIMARY KEY default nextval('user_id'),
+	name varchar(30),
+	password varchar(100))"
+echo "--> Created All Tables."
