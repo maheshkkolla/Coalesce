77518fdb722c435b3a71d4def739b6f4f39218f0
maheshkkolla <mahesh.mk.245@gmail.com>
2015-06-26 12:35:12 +0530

    #T01[Mahehs] Wrote a module to interact with db only insert command is added now

---
+++ own_modules/psql.js
@@ -0,0 +1,56 @@
+var pg = require('pg');
+var INSERT_CMD = "INSERT INTO @TABLE_NAME@(@COLUMNS@) VALUES (@VALUES@)"; 
+
+var getTimeStamp = function() {
+	return("[" + String(new Date()) + "]");
+}
+
+var logError = function(error) {
+	console.log(getTimeStamp() + "Error:", error);
+}
+
+var throwErrorWith = function(errorMessage){
+	throw new Error(getTimeStamp() + errorMessage);
+}
+
+var validateInsertParameters = function(tableName, columns, values) {
+	(tableName && columns && values) || throwErrorWith("Some parameters are missing at insert");
+	(columns.length == values.length) || throwErrorWith("Given columns and values are in different number");
+}
+
+var getVlaues = function(length) {
+	var values = "$1"
+	for(var value=2; value<=length; value++){
+		values += ", $" + value;
+	};
+	return values;
+}
+
+var _insert = function(tableName, columns, values) {
+	validateInsertParameters(tableName, columns, values);
+	this.values = values;
+	this.query = INSERT_CMD.replace(/@TABLE_NAME@/g, tableName); 
+	this.query = this.query.replace(/@COLUMNS@/g, columns.toString()); 
+	this.query = this.query.replace(/@VALUES@/g, getVlaues(values.length)); 
+}
+
+var _exec = function(callback) {
+	var qry = this.query;
+	var values = this.values;
+	pg.connect(this.connStr, function(error, client, done){
+		error && logError(error);
+		client.query(qry, values);
+	});
+}
+
+var Psql = function(host, port, db) {
+	this.query = "";
+	this.connStr = "postgres://" + host + ":" + port + "/" + db;
+}
+
+exports.Psql = Psql;
+
+Psql.prototype = {
+	insert: _insert,
+	exec: _exec
+}
