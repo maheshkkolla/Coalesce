0d4c91d888d2d7d9856112805881a7eb6fe95f5a
maheshkkolla <mahesh.mk.245@gmail.com>
2015-06-27 14:39:47 +0530

    #S02 [Mahesh] Added create organisation link in home page and displaying organisations

---
+++ own_modules/coalsce_modules.js
@@ -0,0 +1,21 @@
+var Psql = require("./psql.js").Psql;
+
+var _getAllOrganisations = function(callback) {
+	var db = new Psql('localhost','5432','coalsce');
+	db.select("organisations");
+	db.exec(function(err, result) {
+		callback(result.rows);
+	});
+}
+
+
+
+
+
+
+
+
+
+module.exports = {
+	getAllOrganisations: _getAllOrganisations
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var coalsce = require("../own_modules/coalsce_modules.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	coalsce.getAllOrganisations( function(organisations) {
+		res.render('index', {organisations: organisations});
+	});
 });
 
 module.exports = router;
