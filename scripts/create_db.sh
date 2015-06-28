23fb13dd40045935891291e92d7a25291332e8e3
Ananthu R V <ananthur@thoughtworks.com>
2015-06-28 12:52:21 +0530

    #nocard changed script for db reset with new database name

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 echo "--> Clearing old data if exits"
-dropdb --if-exists coalsce
+dropdb --if-exists organisations
 echo "--> Creating DataBase ..."
 createdb organisations
 echo "--> Created DataBase."
 echo "--> Creating Tables ..."
-psql coalsce -c "CREATE SEQUENCE org_id MINVALUE 1111"
-psql coalsce -c "CREATE TABLE organisations(
+psql organisations -c "CREATE TABLE organisations(
 	email varchar(254) PRIMARY KEY,
-	name varchar(30)) UNIQUE"
+	name varchar(30) UNIQUE)"
 echo "--> Created All Tables Needed."
