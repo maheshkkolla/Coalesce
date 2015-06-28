c28257608ecc7f547baff369db9f03402aa24d14
maheshkkolla <mahesh.mk.245@gmail.com>
2015-06-28 13:20:56 +0530

    #S02 [Mahesh|Ananthu] Creating Organisations

--- own_modules/organisations_modules.js
+++ own_modules/organisations_modules.js
@@ -3,8 +3,11 @@ var host = "localhost";
 var port = "5432";
 var dataBase = "organisations"
 
-var _create = function(organisation) {
+var _create = function(organisation, callback) {
 	var db = new Psql(host,port,dataBase);
+	var values = [organisation.email, organisation.name, organisation.siteName];
+	db.insert('organisations',['email','name','site_name'],values);
+	db.exec(callback);
 }
 
 module.exports = {
