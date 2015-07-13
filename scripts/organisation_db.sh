889d0cd05fc940bef05f707bec9d52f49c373701
maheshkkolla <mahesh.mk.245@gmail.com>
2015-07-13 14:39:55 +0530

    -m[Mahesh] Added script to create messages table

--- scripts/organisation_db.sh
+++ scripts/organisation_db.sh
@@ -5,8 +5,18 @@ echo "--> Creating DataBase ..."
 createdb coalsce
 echo "--> Created DataBase."
 echo "--> Creating Tables ..."
+psql coalsce -c "CREATE SEQUENCE usr_id"
 psql coalsce -c "CREATE TABLE users(
-	email varchar(254) PRIMARY KEY,
+	id integer PRIMARY KEY default nextval('usr_id'),
+	email varchar(254) UNIQUE,
 	name varchar(30),
 	password varchar(30))"
+psql coalsce -c "CREATE SEQUENCE msg_id"
+psql coalsce -c "CREATE TABLE messages(
+	id integer PRIMARY KEY default nextval('msg_id'),
+	message text,
+	sender integer references users(id),
+	receiver integer references users(id),
+	dt timestamp
+	)"
 echo "--> Created All Tables Needed."
